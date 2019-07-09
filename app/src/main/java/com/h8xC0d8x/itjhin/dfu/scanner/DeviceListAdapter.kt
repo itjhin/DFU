a7014a80874da440c0ceff9744b4c5cd11b0b7da
Ivan Tjhin <i.sutanto@revic.us>
2019-07-09 13:47:37 -0500

    Scanner Fragment logic and resources

---
+++ app/src/main/java/com/h8xC0d8x/itjhin/dfu/scanner/DeviceListAdapter.kt
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 2015, Nordic Semiconductor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.h8xC0d8x.itjhin.dfu.scanner
+
+import android.content.Context
+import android.widget.BaseAdapter
+import android.bluetooth.BluetoothDevice;
+import no.nordicsemi.android.support.v18.scanner.ScanResult;
+import com.h8xC0d8x.itjhin.dfu.R
+import android.widget.TextView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+
+/**
+ * DeviceListAdapter class is list adapter for showing scanned Devices name, address and RSSI image based on RSSI values.
+ */
+class DeviceListAdapter() : BaseAdapter() {
+    private val TYPE_TITLE = 0
+    private val TYPE_ITEM = 1
+    private val TYPE_EMPTY = 2
+
+    private val mListBondedValues = ArrayList<ExtendedBluetoothDevice>()
+    private val mListValues = ArrayList<ExtendedBluetoothDevice>()
+    private var mContext: Context? = null
+
+    constructor(context: Context?) : this() {
+        this.mContext = context
+    }
+
+    /**
+     * Sets a list of bonded devices.
+     * @param devices list of bonded devices.
+     */
+    fun addBondedDevices(devices: Set<BluetoothDevice>) {
+        val bondedDevices = mListBondedValues
+        for (device in devices) {
+            bondedDevices.add(ExtendedBluetoothDevice(device))
+        }
+        notifyDataSetChanged()
+    }
+    /**
+     * Updates the list of not bonded devices.
+     * @param results list of results from the scanner
+     */
+    fun update(results: List<ScanResult>) {
+        for (result in results) {
+            val device = findDevice(result)
+            if (device == null) {
+                mListValues.add(ExtendedBluetoothDevice(result))
+            } else {
+                device!!.name = if (result.scanRecord != null) result.scanRecord!!.deviceName else null
+                device!!.rssi = result.rssi
+            }
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun findDevice(result: ScanResult): ExtendedBluetoothDevice? {
+        for (device in mListBondedValues)
+            if (device.matches(result))
+                return device
+        for (device in mListValues)
+            if (device.matches(result))
+                return device
+        return null
+    }
+
+    fun clearDevices() {
+        mListValues.clear()
+        notifyDataSetChanged()
+    }
+
+    override fun getCount(): Int {
+        val bondedCount = mListBondedValues.size + 1 // 1 for the title
+        val availableCount = if (mListValues.isEmpty()) 2 else mListValues.size+ 1 // 1 for title, 1 for empty text
+        return if (bondedCount == 1) availableCount else bondedCount + availableCount
+    }
+
+
+    override fun getItem(position: Int): Any {
+        val bondedCount = mListBondedValues.size + 1 // 1 for the title
+        if (mListBondedValues.isEmpty()) {
+            return if (position == 0)
+                R.string.scanner_subtitle_not_bonded
+            else
+                mListValues[position - 1]
+        } else {
+            if (position == 0)
+                return R.string.scanner_subtitle_bonded
+            if (position < bondedCount)
+                return mListBondedValues[position - 1]
+            return if (position == bondedCount) R.string.scanner_subtitle_not_bonded else mListValues[position - bondedCount - 1]
+        }
+    }
+
+    override fun getViewTypeCount(): Int {
+        return 3
+    }
+
+    override fun areAllItemsEnabled(): Boolean {
+        return false
+    }
+
+    override fun isEnabled(position: Int): Boolean {
+        return getItemViewType(position) == TYPE_ITEM
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0)
+            return TYPE_TITLE
+
+        if (!mListBondedValues.isEmpty() && position == mListBondedValues.size + 1)
+            return TYPE_TITLE
+
+        return if (position == count - 1 && mListValues.isEmpty()) TYPE_EMPTY else TYPE_ITEM
+
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, oldView: View, parent: ViewGroup): View {
+        val inflater = LayoutInflater.from(mContext)
+        val type = getItemViewType(position)
+
+        var view: View? = oldView
+        when (type) {
+            TYPE_EMPTY -> if (view == null) {
+                view = inflater.inflate(R.layout.device_list_empty, parent, false)
+            }
+            TYPE_TITLE -> {
+                if (view == null) {
+                    view = inflater.inflate(R.layout.device_list_title, parent, false)
+                }
+                val title = view as TextView?
+                title!!.setText(getItem(position) as Int)
+            }
+            else -> {
+                if (view == null) {
+                    view = inflater.inflate(R.layout.device_list_row, parent, false)
+                    val holder = ViewHolder()
+                    holder.name = view!!.findViewById(R.id.name)
+                    holder.address = view!!.findViewById(R.id.address)
+                    holder.rssi = view!!.findViewById(R.id.rssi)
+                    view!!.setTag(holder)
+                }
+
+                val device = getItem(position) as ExtendedBluetoothDevice
+                val holder = view!!.getTag() as ViewHolder
+                val name = device.name
+                holder.name?.setText(name ?: mContext?.getString(R.string.not_available))
+                holder.address?.setText(device.device!!.address)
+                if (!device.isBonded || device.rssi != ExtendedBluetoothDevice.NO_RSSI) {
+                    val rssiPercent = (100.0f * (127.0f + device.rssi) / (127.0f + 20.0f)).toInt()
+                    holder.rssi?.setImageLevel(rssiPercent)
+                    holder.rssi?.setVisibility(View.VISIBLE)
+                } else {
+                    holder.rssi?.setVisibility(View.GONE)
+                }
+            }
+        }
+
+        return view!!
+    }
+
+    private inner class ViewHolder {
+        var name: TextView? = null
+        var address: TextView? = null
+        var rssi: ImageView? = null
+    }
+}
--- app/src/main/java/com/h8xC0d8x/itjhin/dfu/scanner/ScannerFragment.kt
+++ app/src/main/java/com/h8xC0d8x/itjhin/dfu/scanner/ScannerFragment.kt
@@ -1,25 +1,54 @@
 package com.h8xC0d8x.itjhin.dfu.scanner
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import android.os.ParcelUuid
 import java.util.*
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
 import android.content.Context
-//import com.h8xC0d8x.itjhin.dfu.scanner.ScannerFragment.OnDeviceSelectedListener
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.widget.Button
 
 
-
-
-
+import com.h8xC0d8x.itjhin.dfu.R
+import no.nordicsemi.android.support.v18.scanner.BluetoothLeScannerCompat
+import no.nordicsemi.android.support.v18.scanner.ScanCallback
+import no.nordicsemi.android.support.v18.scanner.ScanFilter
+import no.nordicsemi.android.support.v18.scanner.ScanResult
+import no.nordicsemi.android.support.v18.scanner.ScanSettings
+import android.os.Handler
+import android.view.LayoutInflater
+import android.widget.ListView
+import android.widget.Toast
 
 
 
 class ScannerFragment : DialogFragment() {
     private val PARAM_UUID = "param_uuid"
+    private val SCAN_DURATION: Long = 5000
+
     private var mUuid: ParcelUuid? = null
+    private val REQUEST_PERMISSION_REQ_CODE = 34 // any 8-bit number
 
     private var mListener: OnDeviceSelectedListener? = null
+    private var mPermissionRationale: View? = null
+    private var mBluetoothAdapter : BluetoothAdapter? = null
+
+    private var mAdapter: DeviceListAdapter? = null
+    private val mHandler = Handler()
+
+    private var mScanButton: Button? = null
+    private var mIsScanning = false
+
 
     fun getInstance(uuid: UUID?): ScannerFragment {
         val fragment = ScannerFragment()
@@ -65,4 +94,162 @@ class ScannerFragment : DialogFragment() {
         }
 
     }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var args : Bundle? = getArguments();
+        if (args != null && args.containsKey(PARAM_UUID)) {
+            mUuid = args.getParcelable(PARAM_UUID);
+        }
+
+        val manager : BluetoothManager = requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        if (manager != null) {
+            mBluetoothAdapter = manager.getAdapter();
+        }
+    }
+
+    override fun onDestroyView() {
+        stopScan()
+        super.onDestroyView()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder : AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        val dialogView : View = LayoutInflater.from(activity).inflate(R.layout.fragment_device_selection, null)
+        val listview : ListView = dialogView.findViewById(android.R.id.list)
+
+        listview.setEmptyView(dialogView.findViewById(android.R.id.empty))
+        mAdapter = DeviceListAdapter(activity)
+        listview.adapter = mAdapter
+
+        builder.setTitle(R.string.scanner_title)
+        val dialog : AlertDialog = builder.setView(dialogView).create()
+
+        listview.setOnItemClickListener { parent, view, position, id ->
+            stopScan()
+            dialog.dismiss()
+            val d : ExtendedBluetoothDevice = mAdapter!!.getItem(position) as ExtendedBluetoothDevice
+            mListener!!.onDeviceSelected(d.device!!,d.name!!)
+        }
+
+        mPermissionRationale = dialogView.findViewById(R.id.permission_rationale) // this is not null only on API23+
+
+        mScanButton = dialogView.findViewById(R.id.action_cancel)
+        mScanButton!!.setOnClickListener { v ->
+            if (v.id == R.id.action_cancel) {
+                if(mIsScanning) {
+                    dialog.cancel()
+                } else {
+                    startScan()
+                }
+            }
+        }
+
+        addBoundDevices();
+        if (savedInstanceState == null)
+                startScan();
+
+        return dialog;
+        //return super.onCreateDialog(savedInstanceState)
+    }
+
+    override fun onCancel(dialog: DialogInterface?) {
+        super.onCancel(dialog)
+
+        mListener?.onDialogCanceled()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSION_REQ_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // We have been granted the Manifest.permission.ACCESS_COARSE_LOCATION permission. Now we may proceed with scanning.
+                    startScan()
+                } else {
+                    mPermissionRationale?.setVisibility(View.VISIBLE)
+                    Toast.makeText(activity, R.string.no_required_permission, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    /**
+     * Scan for 5 seconds and then stop scanning when a BluetoothLE device is found then mLEScanCallback
+     * is activated This will perform regular scan for custom BLE Service UUID and then filter out.
+     * using class ScannerServiceParser
+     */
+    private fun startScan() {
+        // Since Android 6.0 we need to obtain either Manifest.permission.ACCESS_COARSE_LOCATION or Manifest.permission.ACCESS_FINE_LOCATION to be able to scan for
+        // Bluetooth LE devices. This is related to beacons as proximity devices.
+        // On API older than Marshmallow the following code does nothing.
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // When user pressed Deny and still wants to use this functionality, show the rationale
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) && mPermissionRationale?.visibility == View.GONE) {
+                mPermissionRationale?.visibility = View.VISIBLE
+                return
+            }
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),REQUEST_PERMISSION_REQ_CODE)
+            return
+        }
+
+        // Hide the rationale message, we don't need it anymore.
+        if (mPermissionRationale != null)
+            mPermissionRationale!!.setVisibility(View.GONE)
+
+        mAdapter?.clearDevices()
+        mScanButton?.text = getString(R.string.scanner_action_cancel)
+
+        val scanner = BluetoothLeScannerCompat.getScanner()
+        val settings = ScanSettings.Builder()
+            .setLegacy(false)
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).setReportDelay(1000)
+            .setUseHardwareBatchingIfSupported(false).build()
+
+        val filters = ArrayList<ScanFilter>()
+        filters.add(ScanFilter.Builder().setServiceUuid(mUuid).build())
+        scanner.startScan(filters, settings, scanCallback)
+
+        mIsScanning = true
+        mHandler.postDelayed({
+            if (mIsScanning) {
+                stopScan()
+            }
+        }, SCAN_DURATION)
+
+    }
+
+    /**
+     * Stop scan if user tap Cancel button
+     */
+    fun stopScan() {
+        if (mIsScanning) {
+            mScanButton?.text= getText(R.string.scanner_action_scan)
+            val scanner = BluetoothLeScannerCompat.getScanner()
+            scanner.stopScan(scanCallback)
+
+            mIsScanning = false
+        }
+    }
+
+    fun addBoundDevices() {
+        val devices : Set<BluetoothDevice> = mBluetoothAdapter!!.bondedDevices
+        mAdapter?.addBondedDevices(devices)
+    }
+
+    private var scanCallback : ScanCallback = object: ScanCallback() {
+
+        override fun onScanResult(callbackType : Int, result : ScanResult) {
+            // do nothing
+        }
+
+        override fun onBatchScanResults(results : List<ScanResult> ) {
+            mAdapter?.update(results);
+        }
+
+        override fun onScanFailed(errorCode : Int) {
+            // should never be called
+        }
+    }
+
 }
